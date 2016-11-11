156344646aab67383e9bf78cb6fdf654158cd7d3
Galileo Teco <g@80bits.com>
2016-11-11 14:10:23 -0600

    added sendMessage

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var update = require('./routes/update');
+var sendMessage = require('./routes/sendMessage');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/api/update', update);
+app.use('/api/sendMessage', sendMessage);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
