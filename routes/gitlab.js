f1180f25af65e8553b66c0702a7a735215e43b67
Galileo Teco <g@80bits.com>
2017-03-23 22:16:03 -0600

    it was added url into message

--- routes/gitlab.js
+++ routes/gitlab.js
@@ -7,7 +7,8 @@ router.post('/PushEvents/:ChatId', function(req, res, next) {
     var project = ['*Project: *', req.body.project.name, '\n'].join('');
     var totalCommits = ['*Total Commits: *', req.body.total_commits_count, '\n'].join('');
     var branch = ['*Branch: *', req.body.ref.replace('refs/heads/', ''), '\n'].join('');
-    var text = [object_kind, author, project, branch, totalCommits].join("");
+    var urlProject = ['*Url: *', req.body.repository.homepage].join('');
+    var text = [object_kind, author, project, branch, totalCommits, urlProject].join("");
     var Model = {
         ChatId: req.params.ChatId,
         Header: "Push Event",
