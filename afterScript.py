20d68db75c48eed902f6abf2623ce2a3eca376ea
Galileo Teco <g@80bits.com>
2019-02-18 02:30:11 -0600

    I added files for CI, Upload it to azure

---
+++ afterScript.py
@@ -0,0 +1,21 @@
+import requests
+import datetime
+import json
+import os
+
+
+url = os.environ['PAYNAL_BOT_URL']
+
+payloadModel = {
+  'Header': 'Guigo FrontApi\n',
+  'Body': 'Deploy Done\n' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+}
+
+payload = json.dumps(payloadModel)
+
+headers = {
+    'Content-Type': "application/json",
+    'cache-control': "no-cache",
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers)
